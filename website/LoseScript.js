3d9ab13401358ac884f67cf2fb981b5556f56ccc
Daniel <donybof1@gmail.com>
2024-03-08 12:02:14 +0200

    Dvirs report

--- website/LoseScript.js
+++ website/LoseScript.js
@@ -1,44 +1,50 @@
-function lose(counter) {
-    window.location.href = "lose.html";
-    console.log(counter);
+function lose(points) {
+    window.location.href = "./lose.html";
+    var pointsdiv = document.getElementById("points");
+    pointsdiv.innerText = points;
 }
-
 function CheckUserNameValid() {
-    var name = document.getElementById("Username");
-    if (name.value != null) {
-        AddToScoreBoard(name.value, counter);
-        window.location.href = "scoreboard.html";
-        handleScoreboardClick();
+    var points = document.getElementById("points").value;
+    var name = document.getElementById("Username").value;
+    if (name != "" && name != null) {
+        window.location.href = "./scoreboard.html";
+        addPlayer(name, points);
+        handlePlayersClick();
     }
 }
-async function AddToScoreBoard(name, counter) {
-    console.log("Adding player to scoreboard");
-    let player = {
-        name: name,
-        points: counter
-    };
-    await fetch("/addPlayer", {
-        method: "POST",
-        body: JSON.stringify(player),
-    });
+var table =  document.getElementById("table");
+ async function handlePlayersClick() {
+      let response = await fetch("/getPlayers");
+      let json = await response.json();
+     console.log(json);
+     
+     table.innerHTML = null;
+     for (let i = 0; i < json.length; i++) {
+         let row = document.createElement('tr');
+         let nameCell = document.createElement('td')
+         nameCell.appendChild(document.createTextNode(json[i].name));
+         row.appendChild(nameCell);
+        let pointsCell = document.createElement("td");
+     player.appendChild(pointsCell);
+         pointsCell.innerText = json.points;
+        row.appendChild(pointsCell);
+         table.appendChild(row);
+     }
+
 }
-async function handleScoreboardClick() {
-    let response = await fetch("/getPlayers");
-    let json = await response.json();
-    console.log(json[0]);
+    async function addPlayer(name, points) {
+            let player = {
+                name: name,
+                points: points
+            };
 
 
-    var scoreboard = document.getElementById("scoreboardDIV");
-    var table = document.createElement("table");
-    table.innerHTML = null;
-    for (var i = 0; i < json.length; i++) {
-        var row = document.createElement("tr");
-        var name = document.createElement("td");
-        var points = document.createElement("td");
-        name.innerText = json[i].name;
-        points.innerText = json[i].points;
-        row.appendChild(name);
-    }
-    table.appendChild(row);
-    scoreboard.appendChild(table);
-}
+            let playetString = JSON.stringify(player);
+
+
+            await fetch("/addPlayer", {
+                method: "POST",
+                body: playetString,
+            });
+        }
+
